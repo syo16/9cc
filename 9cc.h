675ff5663b6ee4af27ccdb6b0cb3f282c8056cfb
大崎祥一郎 <syo16_omt_happy@outlook.jp>
2020-04-25 09:57:23 +0900

    ファイルを分割

---
+++ 9cc.h
@@ -0,0 +1,59 @@
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+    TK_RESERVED,
+    TK_NUM,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    long val;
+    char *str;
+    int len;
+};
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+long expect_number(void);
+bool at_eof(void);
+Token *tokenize(void);
+
+extern char *user_input;
+extern Token *token;
+
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_EQ,  // ==
+    ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
+    ND_NUM, // Integer
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    long val;
+};
+
+
+Node *expr(void);
+
+void codegen(Node *node);
---
+++ main.c
@@ -0,0 +1,14 @@
+#include "9cc.c"
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        error("引数の個数が不正です");
+    }
+
+    user_input = argv[1];
+    token = tokenize();
+    Node *node = expr();
+
+    codegen(node);
+    return 0;
+}
